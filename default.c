c43f58247d0a4afa73af6efbb8af0758109bb203
Snowy1794 <391483542@qq.com>
2018-09-26 01:39:16 +0800

    update 2018/09/26

---
+++ default.c
@@ -0,0 +1,290 @@
+	/* common: professions */
+	lang[LANG_COMMON_PROF_GUARDIAN] = "Gdn";
+	lang[LANG_COMMON_PROF_DRAGONHUNTER] = "Dgh";
+	lang[LANG_COMMON_PROF_WARRIOR] = "War";
+	lang[LANG_COMMON_PROF_BERSERKER] = "Brs";
+	lang[LANG_COMMON_PROF_ENGINEER] = "Eng";
+	lang[LANG_COMMON_PROF_SCRAPPER] = "Scr";
+	lang[LANG_COMMON_PROF_RANGER] = "Rgr";
+	lang[LANG_COMMON_PROF_DRUID] = "Dru";
+	lang[LANG_COMMON_PROF_THIEF] = "Thf";
+	lang[LANG_COMMON_PROF_DAREDEVIL] = "Dar";
+	lang[LANG_COMMON_PROF_ELEMENTALIST] = "Ele";
+	lang[LANG_COMMON_PROF_TEMPEST] = "Tmp";
+	lang[LANG_COMMON_PROF_MESMER] = "Mes";
+	lang[LANG_COMMON_PROF_CHRONOMANCER] = "Chr";
+	lang[LANG_COMMON_PROF_NECROMANCER] = "Nec";
+	lang[LANG_COMMON_PROF_REAPER] = "Rea";
+	lang[LANG_COMMON_PROF_RECKONER] = "Rev";
+	lang[LANG_COMMON_PROF_HERALD] = "Her";
+	lang[LANG_COMMON_PROF_FIREBRAND] = "Fbd";
+	lang[LANG_COMMON_PROF_SPELLBREAKER] = "Spb";
+	lang[LANG_COMMON_PROF_HOLOSMITH] = "Hls";
+	lang[LANG_COMMON_PROF_SOULBEAST] = "Slb";
+	lang[LANG_COMMON_PROF_DEADEYE] = "Ded";
+	lang[LANG_COMMON_PROF_WEAVER] = "Wea";
+	lang[LANG_COMMON_PROF_MIRAGE] = "Mir";
+	lang[LANG_COMMON_PROF_SCOURGE] = "Scg";
+	lang[LANG_COMMON_PROF_RENEGADE] = "Ren";
+
+	/* common: skill itemization */
+	lang[LANG_COMMON_SKILLS_ACTIVATIONS] = "activations";
+	lang[LANG_COMMON_SKILLS_CANCELS] = "cancels";
+	lang[LANG_COMMON_SKILLS_WASTED] = "time wasted";
+	lang[LANG_COMMON_SKILLS_HITSCOMPRESS] = "hits (cmprs)";
+	lang[LANG_COMMON_SKILLS_HITSTOTAL] = "hits (dmg)";
+	lang[LANG_COMMON_SKILLS_NINETY] = "over 90";
+	lang[LANG_COMMON_SKILLS_FIFTY] = "under 50";
+	lang[LANG_COMMON_SKILLS_MOVING] = "moving";
+	lang[LANG_COMMON_SKILLS_FLANKING] = "flanking";
+	lang[LANG_COMMON_SKILLS_CRITS] = "crit";
+	lang[LANG_COMMON_SKILLS_VALUE] = "total";
+	lang[LANG_COMMON_SKILLS_MIN] = "min";
+	lang[LANG_COMMON_SKILLS_AVG] = "avg";
+	lang[LANG_COMMON_SKILLS_MAX] = "max";
+	lang[LANG_COMMON_SKILLS_APXDMG] = "apx dmg";
+	lang[LANG_COMMON_SKILLS_OVERSTACK] = "overstack";
+	lang[LANG_COMMON_SKILLS_NOSKILLS] = "No skills logged";
+	lang[LANG_COMMON_SKILLS_REMOVED] = "removed";
+	lang[LANG_COMMON_SKILLS_RESISTED] = "resisted";
+	lang[LANG_COMMON_SKILLS_GLANCE] = "glance";
+	lang[LANG_COMMON_SKILLS_MISS] = "miss";
+	lang[LANG_COMMON_SKILLS_NOTARGETS] = "No targets logged";
+
+	/* common: skills 2 */
+	lang[LANG_COMMON_SKILLS2_NOEVENTS] = "No events";
+	lang[LANG_COMMON_SKILLS2_SHIELD] = "shielded";
+	lang[LANG_COMMON_SKILLS2_HITSANY] = "hits (all)";
+	lang[LANG_COMMON_SKILLS2_DMGPHYS] = "dmg (phys)";
+	lang[LANG_COMMON_SKILLS2_DMGBUFF] = "dmg (buff)";
+	lang[LANG_COMMON_SKILLS2_CHANNELED] = "time cast";
+	lang[LANG_COMMON_SKILLS2_PROTECTED] = "protected";
+	lang[LANG_COMMON_SKILLS2_KILLINGBLOWS] = "killed";
+	lang[LANG_COMMON_SKILLS2_DOWNEDS] = "downed";
+
+	/* common: custom skills */
+	lang[LANG_COMMON_CSK_DODGE] = "Dodge";
+	lang[LANG_COMMON_CSK_SHIELD] = "Barrier";
+
+	/* panels: dps */
+	lang[LANG_PAN_PERSONALDPS_HEADER] = "Self Stats";
+	lang[LANG_PAN_PERSONALDPS_DAMAGE] = "damage";
+	lang[LANG_PAN_PERSONALDPS_HEALING] = "healing";
+	lang[LANG_PAN_PERSONALDPS_HISTLEFT] = "current";
+	lang[LANG_PAN_PERSONALDPS_HISTRIGHT] = "current";
+
+	/* panels: skills */
+	lang[LANG_PAN_PERSONALSKILLS_HEADER] = "Self Skills";
+
+	/* panels: healthbar */
+	lang[LANG_PAN_HEALTHBAR_HEADER] = "Healthbar";
+
+	/* panels: chcli */
+	lang[LANG_PAN_CHCLI_HEADER] = "Area Stats";
+	lang[LANG_PAN_CHCLI_DISABLED] = "Disabled on out-of-party players";
+	lang[LANG_PAN_CHCLI_NOPLAYERS] = "No players in range";
+	lang[LANG_PAN_CHCLI_HISTLEFT] = "current";
+	lang[LANG_PAN_CHCLI_HISTRIGHT] = "current";
+
+	/* panels: summary */
+	lang[LANG_PAN_SUMMARY_HEADER] = "Summary";
+	lang[LANG_PAN_SUMMARY_CBTTIME] = "combat time";
+	lang[LANG_PAN_SUMMARY_ADPSOUT] = "dps out";
+	lang[LANG_PAN_SUMMARY_TGADPSOUT] = "tg dps out";
+	lang[LANG_PAN_SUMMARY_ADPSIN] = "dps in";
+	lang[LANG_PAN_SUMMARY_TGADPSIN] = "tg dps in";
+	lang[LANG_PAN_SUMMARY_CRIT] = "crit rate";
+	lang[LANG_PAN_SUMMARY_NINETY] = "ninety rate";
+	lang[LANG_PAN_SUMMARY_FIFTY] = "fifty rate";
+	lang[LANG_PAN_SUMMARY_MOVING] = "moving rate";
+	lang[LANG_PAN_SUMMARY_FLANK] = "flank rate";
+	lang[LANG_PAN_SUMMARY_CANCELTIME] = "cancel time";
+	lang[LANG_PAN_SUMMARY_RESCOUNT] = "res count";
+	lang[LANG_PAN_SUMMARY_RESTIME] = "res time";
+	lang[LANG_PAN_SUMMARY_DOWNCOUNT] = "down count";
+	lang[LANG_PAN_SUMMARY_CLEANSEDOUT] = "cleanse out";
+	lang[LANG_PAN_SUMMARY_STRIPPEDOUT] = "strip out";
+	lang[LANG_PAN_SUMMARY_GLANCE] = "glance rate";
+	lang[LANG_PAN_SUMMARY_EVADES] = "evades";
+	lang[LANG_PAN_SUMMARY_INTERRUPTS] = "interrupts";
+	lang[LANG_PAN_SUMMARY_MISSED] = "miss rate";
+	lang[LANG_PAN_SUMMARY_DODGE] = "dodges used";
+	lang[LANG_PAN_SUMMARY_HIST] = "History";
+	lang[LANG_PAN_SUMMARY_SQSHORT1] = "player must be";
+	lang[LANG_PAN_SUMMARY_SQSHORT2] = "in party/squad";
+	lang[LANG_PAN_SUMMARY_CLEANSEDIN] = "cleanse in";
+	lang[LANG_PAN_SUMMARY_STRIPPEDIN] = "strip in";
+	lang[LANG_PAN_SUMMARY_DOWNTIME] = "down time";
+	lang[LANG_PAN_SUMMARY_CANCELCOUNT] = "cancel count";
+	lang[LANG_PAN_SUMMARY_SHIELDPCT] = "shield pct";
+	lang[LANG_PAN_SUMMARY_SHIELDEFF] = "shield eff";
+	lang[LANG_PAN_SUMMARY_BLOCKS] = "blocks";
+	lang[LANG_PAN_SUMMARY_ABSORBS] = "absorbs";
+	lang[LANG_PAN_SUMMARY_CLEANSEDSELF] = "cleanse self";
+	lang[LANG_PAN_SUMMARY_DURATION] = "duration";
+	lang[LANG_PAN_SUMMARY_INTENSITY] = "intensity";
+	lang[LANG_PAN_SUMMARY_PHYS] = "phys";
+	lang[LANG_PAN_SUMMARY_COND] = "cond";
+	lang[LANG_PAN_SUMMARY_DPSUPTIME] = "dps uptime";
+	lang[LANG_PAN_SUMMARY_PROT] = "prot rate";
+	lang[LANG_PAN_SUMMARY_KILLINGBLOWS] = "killed";
+	lang[LANG_PAN_SUMMARY_DOWNEDS] = "downed";
+
+	/* panels: logger */
+	lang[LANG_PAN_LOGGER_HEADER] = "Log";
+	lang[LANG_PAN_LOGGER_CBT] = "cbt";
+	lang[LANG_PAN_LOGGER_ENG] = "eng";
+	lang[LANG_PAN_LOGGER_GME] = "gme";
+	lang[LANG_PAN_LOGGER_FILTER] = "filter";
+	lang[LANG_PAN_LOGGER_RESQ] = "res.qdep";
+	lang[LANG_PAN_LOGGER_CBTQ] = "cbt.qdep";
+	lang[LANG_PAN_LOGGER_MBLK] = "m.blks";
+	lang[LANG_PAN_LOGGER_MSZ] = "m.sz";
+	lang[LANG_PAN_LOGGER_AG] = "lim.ag";
+	lang[LANG_PAN_LOGGER_CH] = "lim.ch";
+	lang[LANG_PAN_LOGGER_CEA] = "ces";
+	lang[LANG_PAN_LOGGER_GRC] = "grc";
+	lang[LANG_PAN_LOGGER_CBS] = "cbs";
+	lang[LANG_PAN_LOGGER_CCX] = "ccx";
+	lang[LANG_PAN_LOGGER_TGC] = "tgc";
+	lang[LANG_PAN_LOGGER_SCR] = "scr";
+	lang[LANG_PAN_LOGGER_RTH] = "rth";
+	lang[LANG_PAN_LOGGER_CCS] = "ccs";
+	lang[LANG_PAN_LOGGER_BCX] = "bcx";
+	lang[LANG_PAN_LOGGER_GPT] = "gpt";
+	lang[LANG_PAN_LOGGER_RND] = "rnd";
+	lang[LANG_PAN_LOGGER_REF] = "upk";
+	lang[LANG_PAN_LOGGER_CHANNEL] = "Channel";
+	lang[LANG_PAN_LOGGER_SIM] = "sim";
+	lang[LANG_PAN_LOGGER_MCI] = "mci";
+	lang[LANG_PAN_LOGGER_MCO] = "mco";
+	lang[LANG_PAN_LOGGER_LINES] = "lines";
+	lang[LANG_PAN_LOGGER_SUPPRESS] = "suppress logging";
+	lang[LANG_PAN_LOGGER_PERFTIME] = "perf.time";
+	lang[LANG_PAN_LOGGER_PERFUSE] = "perf.use";
+	lang[LANG_PAN_LOGGER_EVENTS] = "Events";
+	lang[LANG_PAN_LOGGER_CBTSTATECHANGE] = "statechange";
+	lang[LANG_PAN_LOGGER_CBTACTIVATION] = "activation";
+	lang[LANG_PAN_LOGGER_CBTBUFFREMOVE] = "buffremove";
+	lang[LANG_PAN_LOGGER_CBTBUFFAPPLY] = "buffapply";
+	lang[LANG_PAN_LOGGER_CBTBUFFDAMAGE] = "buffdamage";
+	lang[LANG_PAN_LOGGER_CBTBUFFHEAL] = "buffheal";
+	lang[LANG_PAN_LOGGER_CBTDIRECTDAMAGE] = "directdamage";
+	lang[LANG_PAN_LOGGER_CBTDIRECTHEAL] = "directheal";
+	lang[LANG_PAN_LOGGER_DEBUG] = "Debug";
+
+	/* panels: compass */
+	lang[LANG_PAN_COMPASS_HEADER] = "Compass";
+	lang[LANG_PAN_COMPASS_N] = "N";
+	lang[LANG_PAN_COMPASS_E] = "E";
+	lang[LANG_PAN_COMPASS_S] = "S";
+	lang[LANG_PAN_COMPASS_W] = "W";
+
+	/* panels: metrics */
+	lang[LANG_PAN_METRICS_HEADER] = "Metrics";
+	lang[LANG_PAN_METRICS_FPS] = "F";
+	lang[LANG_PAN_METRICS_PING] = "P";
+	lang[LANG_PAN_METRICS_MAPTYPE] = "Map type";
+	lang[LANG_PAN_METRICS_MAPID] = "Map ID";
+	lang[LANG_PAN_METRICS_MAPLV] = "Map level";
+	lang[LANG_PAN_METRICS_BUILDGW] = "GW build";
+	lang[LANG_PAN_METRICS_BUILDARC] = "Arc build";
+	lang[LANG_PAN_METRICS_TICK] = "R";
+
+	/* panels: gathering */
+	lang[LANG_PAN_GATHER_HEADER] = "Gathering";
+	lang[LANG_PAN_GATHER_PICKH] = "picks/h";
+	lang[LANG_PAN_GATHER_AXEH] = "axes/h";
+	lang[LANG_PAN_GATHER_SICKLEH] = "sickles/h";
+	lang[LANG_PAN_GATHER_PICKED] = "picked";
+	lang[LANG_PAN_GATHER_AXED] = "axed";
+	lang[LANG_PAN_GATHER_SICKLED] = "sickled";
+	lang[LANG_PAN_GATHER_TOOLS] = "tools";
+	lang[LANG_PAN_GATHER_PICKREMAIN] = "picks remaining";
+	lang[LANG_PAN_GATHER_AXEREMAIN] = "axes remaining";
+	lang[LANG_PAN_GATHER_SICKLEREMAIN] = "sickles remaining";
+	lang[LANG_PAN_GATHER_TIME] = "measuring for";
+
+	/* panels: buffs */
+	lang[LANG_PAN_BUFFS_HEADER] = "Buff Table";
+	lang[LANG_PAN_BUFFS_WARNING] = "WARNING: CPU HEAVY";
+
+	/* panels: detail */
+	lang[LANG_PAN_DETAIL_HEADER] = "Detail";
+	lang[LANG_PAN_DETAIL_MIN] = "min";
+	lang[LANG_PAN_DETAIL_MAX] = "max";
+	lang[LANG_PAN_DETAIL_ELAPSED] = "elapsed";
+
+	/* panels: options */
+	lang[LANG_PAN_OPTIONS_HEADER] = "Arcdps Options";
+	lang[LANG_PAN_OPTIONS_PANELS] = "Windows";
+	lang[LANG_PAN_OPTIONS_SUMMARIES] = "Tooltip";
+	lang[LANG_PAN_OPTIONS_BASIC] = "General";
+	lang[LANG_PAN_OPTIONS_SM_ADPSOUT] = "dps out";
+	lang[LANG_PAN_OPTIONS_SM_ADPSIN] = "dps in";
+	lang[LANG_PAN_OPTIONS_SM_BUFFS] = "buffs";
+	lang[LANG_PAN_OPTIONS_SM_RATES] = "rates";
+	lang[LANG_PAN_OPTIONS_SM_AGSTATES] = "states";
+	lang[LANG_PAN_OPTIONS_SM_BUFFEVENTS] = "buff events";
+	lang[LANG_PAN_OPTIONS_SM_RESULTS] = "results";
+	lang[LANG_PAN_OPTIONS_BS_LOCKTARGET] = "Lock target to encounter NPC";
+	lang[LANG_PAN_OPTIONS_BS_SAVELOGS] = "Save EVTC logs after encounters";
+	lang[LANG_PAN_OPTIONS_BS_NPCINPATH] = "Use NPC name in EVTC save path";
+	lang[LANG_PAN_OPTIONS_BS_COMPRESS] = "Compress EVTCs with PowerShell (Win10)";
+	lang[LANG_PAN_OPTIONS_BS_GUILDINPATH] = "Use guild in EVTC save path";
+	lang[LANG_PAN_OPTIONS_BS_BGBARS] = "Draw bars";
+	lang[LANG_PAN_OPTIONS_BS_PLAYERINPATH] = "Use player name in EVTC save path";
+	lang[LANG_PAN_OPTIONS_BS_ALTUIMOVELOCK] = "Moving requires modifiers";
+	lang[LANG_PAN_OPTIONS_BS_ALTUICLICKLOCK] = "Clicking requires modifiers";
+	lang[LANG_PAN_OPTIONS_BS_LOGGING] = "Logging";
+	lang[LANG_PAN_OPTIONS_SM_HIDEZEROBUFFS] = "hide empty buffs";
+	lang[LANG_PAN_OPTIONS_BS_FASTCLOSE] = "Esc closes windows";
+	lang[LANG_PAN_OPTIONS_BS_LOGS] = "Logs (open folder)";
+	lang[LANG_PAN_OPTIONS_BS_CONFIG] = "Config (open folder)";
+
+	/* common assorted out of lazy */
+	lang[LANG_COMMON_AS_SUCCESS] = "success";
+	lang[LANG_COMMON_AS_FAIL] = "failed";
+	lang[LANG_COMMON_AS_PADDING] = "padding";
+	lang[LANG_COMMON_AS_BACKGROUND] = "background";
+	lang[LANG_COMMON_AS_ONELINE] = "one line";
+	lang[LANG_COMMON_AS_ALIGNRIGHT] = "align right";
+	lang[LANG_COMMON_AS_RESET] = "Reset";
+	lang[LANG_COMMON_AS_TOTAL] = "total";
+	lang[LANG_COMMON_AS_CLEAVE] = "cleave";
+	lang[LANG_COMMON_AS_TARGET] = "target";
+	lang[LANG_COMMON_AS_IN] = "in";
+	lang[LANG_COMMON_AS_TITLE] = "title bar";
+	lang[LANG_COMMON_AS_NOHISTORY] = "no history";
+	lang[LANG_COMMON_AS_AUTOSIZE] = "auto size";
+	lang[LANG_COMMON_AS_LISTLENGTH] = "list length";
+	lang[LANG_COMMON_AS_SCROLLBAR] = "scroll bar";
+	lang[LANG_COMMON_AS_FRIENDLY] = "friendly";
+	lang[LANG_COMMON_AS_LIST] = "List";
+	lang[LANG_COMMON_AS_SKILLS] = "skills";
+	lang[LANG_COMMON_AS_TARGETS] = "targets";
+	lang[LANG_COMMON_AS_LOG] = "log";
+	lang[LANG_COMMON_AS_COMBATTIME] = "combat time";
+	lang[LANG_COMMON_AS_SORTDPS] = "sort per second";
+	lang[LANG_COMMON_AS_STYLE] = "Style";
+	lang[LANG_COMMON_AS_BUFFS] = "buffs";
+	lang[LANG_COMMON_AS_SORTTARGET] = "sort vs target";
+	lang[LANG_COMMON_AS_DISPLAY] = "Display";
+	lang[LANG_COMMON_AS_COLOURSUBGROUP] = "colour by subgroup";
+	lang[LANG_COMMON_AS_FORMATTOOLTIP] = "@1: cleave total\n@2: cleave per second\n@3: cleave percent\n@4: target total\n@5: target per second\n@6: target percent";
+	lang[LANG_COMMON_AS_PLAYERFORMAT] = "player format";
+	lang[LANG_COMMON_AS_DEBUG] = "debug";
+	lang[LANG_COMMON_AS_DATA] = "Data";
+	lang[LANG_COMMON_AS_LABELS] = "labels";
+	lang[LANG_COMMON_AS_NUMBERS] = "index numbers";
+	lang[LANG_COMMON_AS_PROF] = "profession";
+	lang[LANG_COMMON_AS_TOPSKILLS] = "top skills";
+	lang[LANG_COMMON_AS_TOPTARGETS] = "top targets";
+	lang[LANG_COMMON_AS_WIDTH] = "window width";
+	lang[LANG_COMMON_AS_DAMAGE] = "Damage";
+	lang[LANG_COMMON_AS_TAKEN] = "taken";
+	lang[LANG_COMMON_AS_TITLEMATCHFRAME] = "use window colour for title bar";
+	lang[LANG_COMMON_AS_TITLEFORMATTOOLTIP] = "@1: cleave per second\n@2: cleave total\n@3: target per second\n@4: target total\n@5: target name";
+	lang[LANG_COMMON_AS_TITLEFORMAT] = "title format";
+	lang[LANG_COMMON_AS_INTERVAL] = "interval";
+	lang[LANG_COMMON_AS_NAMECHARWIDTH] = "name length";
